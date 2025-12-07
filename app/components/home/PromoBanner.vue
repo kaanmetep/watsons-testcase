4cd7b0324761737c080326ff897f0b9349f10fd8
Kaan P. <kaanpmete@gmail.com>
2025-12-07 03:50:33 +0300

    fix: fix a bug with horizontol scroll

--- app/components/home/PromoBanner.vue
+++ app/components/home/PromoBanner.vue
@@ -31,7 +31,7 @@
   </div>
   <!-- Banner Section -->
   <div
-    class="banner-bg w-full py-[35px] sm:py-[55px] flex justify-center sm:items-center"
+    class="banner-bg w-full py-[35px] sm:py-[55px] flex justify-center sm:items-center overflow-x-hidden"
   >
     <div class="w-full max-w-[1160px] mx-auto flex justify-center">
       <div
